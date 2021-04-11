44078e19ad65a5a8ced3d6600b70f7fe1bb62944
rainakmajumder <81399572+rainakmajumder@users.noreply.github.com>
2021-04-11 14:30:38 +0530

    Add files via upload

---
+++ src/test/java/com/pg/warrior/payee/service/PayeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg.warrior.payee.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
